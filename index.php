5f056848e25f916fecf06c00ba46f8d195420873
Genesis <gevaldez@csumb.edu>
2018-02-05 07:15:25 +0000

    Add all lab files

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+First Website
+and comment
+in html
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8"/>
+        <title>Genesis Valdez: Personal Website</title>
+        
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+   <body>
+        
+        
+        <header>
+            <h1>Genesis Valdez</h1>
+        </header>
+        
+        <nav>
+            <hr width="50%"/>
+            
+            <a href="index.php">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        
+        
+        <main>
+            <figure id="me">
+                <img src= "img/genesis.png"/>
+            </figure>
+            <div id="WelcomeText">
+                Hello! <br/>
+                <p>
+                    Thank you for visiting my professional protfolio.
+                </p>
+                <p>
+                    I am a Software Engineer
+                </p>
+                <p>
+                    Feel free to contact me!
+                </p>
+                <br /><br />
+                
+                <em>
+                    "With ordinary talent and extraordinary
+                    <strong>pereverance</strong>,all things are attainable" </em><br />
+                -Thomas F. Buxton
+            </div>
+        </main>
+        <footer>
+            <hr>
+            CST336 Internet Programing.2017
+            <img src= "https://sites.google.com/site/csitin3/_/rsrc/1343759392145/config/Otter.JPG">
+            
+            
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
